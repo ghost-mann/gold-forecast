d0621d5dac54c8e8e01e470453d205737f1eac98
ghost-mann <iamaob8@protonmail.com>
2025-06-20 11:44:05 +0300

    feat: dataframe transformation in main.py

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import pandas as pd
-import prophet
-import matplotlib as plt
+from prophet import Prophet
+import matplotlib.pyplot as plt
 
 # read and print df containing csv
 df = pd.read_csv("data.csv")
 
+# clean price column
+df['Price'] = df['Price'].str.replace(',','').astype(float)
+
+# convert to datetime
+df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+
+# rename colums
+df = df.rename(columns={'Date':'ds','Price':'y'})
+
 # initialize prophet model
 
-model = prophet()
+model = Prophet(daily_seasonality=True)
 model.fit(df)
 
 # future predictions
-future = model.make_future_dataframe(period=365)
+future = model.make_future_dataframe(periods=365)
 
 forecast = model.predict(future)
 
+
+# plot the forecast 
+model.plot(forecast)
+plt.title("XAUUSD Price Forecast")
+plt.show()
