9503f96be7827b85777b653186a03518cca3acfe
ghost-mann <iamaob8@protonmail.com>
2025-06-20 11:32:53 +0300

    feat: initialized prophet model to make future predicitons in main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import pandas as pd
 import prophet
 import matplotlib as plt
+
+# read and print df containing csv
+df = pd.read_csv("data.csv")
+
+# initialize prophet model
+
+model = prophet()
+model.fit(df)
+
+# future predictions
+future = model.make_future_dataframe(period=365)
+
+forecast = model.predict(future)
+
